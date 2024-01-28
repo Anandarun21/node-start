972eda55a5c1d90dd01f5f9b8f1292d6eec50079
Anandarun21 <anandarun238@gmail.com>
2024-01-28 11:57:19 +0530

    spread

--- jsrevise.js
+++ jsrevise.js
@@ -10,4 +10,34 @@ const person ={
         console.log('hi, i am ' + this.name);
     }
 };
-person.greet();
+
+
+person.greet(); 
+const hobbies = ['sports','cookies'];
+// for(let hobby of hobbies){
+//     console.log(hobby);
+// }
+
+console.log(hobbies.map(hobby => 'hobby:' + hobby));
+console.log(hobbies);
+
+
+
+const fruits = ['apple', 'oranges', ' ', 'mango', ' ', 'lemon'];
+
+const transformedFruits = fruits.map(fruit => {
+  return fruit === ' ' ? 'empty string' : fruit;
+});
+
+console.log(transformedFruits);
+
+
+
+
+const obj1 = {'key1': 1, 'key2': 2}
+const obj2 = { ...obj1, key1: 1000 }
+
+console.log(obj1)
+console.log(obj2)
+
+  
