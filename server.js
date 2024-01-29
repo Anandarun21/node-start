8db8ee55d4af4b9d513b5ea0479e2ddbbfd02c12
Anandarun21 <anandarun238@gmail.com>
2024-01-29 15:54:02 +0530

    req/res

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+    console.log(req.url, req.method, req.headers);
 
-    console.log('I am Anand')
+    res.setHeader('Content-Type', 'text/html');
 
+    if (req.url === '/home') {
+        res.write('<html>');
+        res.write('<head><title>Welcome Home</title></head>');
+        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('</html>');
+        res.end();
+    } else if (req.url === '/about') {
+        res.write('<html>');
+        res.write('<head><title>About Us</title></head>');
+        res.write('<body><h1>Welcome to About Us page</h1></body>');
+        res.write('</html>');
+        res.end();
+    } else if (req.url === '/node') {
+        res.write('<html>');
+        res.write('<head><title>Node.js Project</title></head>');
+        res.write('<body><h1>Welcome to my Node.js project</h1></body>');
+        res.write('</html>');
+        res.end();
+    } else {
+        res.write('<html>');
+        res.write('<head><title>Not Found</title></head>');
+        res.write('<body><h1>404 Not Found</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
 });
 
-server.listen(3000);
+server.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
